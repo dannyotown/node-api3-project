a11ebb1762f5ccc1ba6cf8c832979c0c82425834
Dtown <dannyotown@gmail.com>
2019-12-10 11:03:00 -0600

    user api done

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const userDb = require("./userDb");
+const postDb = require("../posts/postDb");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/", validateUser(), async (req, res) => {
   // do your magic!
+  const postUser = await userDb.insert({ name: req.name });
+  res.status(201).send(postUser);
 });
 
-router.post("/:id/posts", (req, res) => {
-  // do your magic!
-});
+router.post(
+  "/:id/posts",
+  validateUserId(),
+  validatePost(),
+  async (req, res) => {
+    const postBody = {
+      text: req.text,
+      user_id: req.params.id
+    };
+    const postToDb = await postDb.insert(postBody);
+    res.status(201).send(postToDb);
+  }
+);
 
 router.get("/", async (req, res) => {
   // do your magic!
@@ -27,12 +40,18 @@ router.get("/:id/posts", validateUserId(), async (req, res) => {
   res.status(200).send(getUserPost);
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", validateUserId(), async (req, res) => {
   // do your magic!
+  await userDb.remove(req.params.id);
+  res.status(201).send(await userDb.getById(req.params.id));
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", validateUserId(), async (req, res) => {
   // do your magic!
+  await userDb.update(req.params.id, {
+    name: req.body.name
+  });
+  res.status(201).send(await userDb.getById(req.params.id));
 });
 
 //custom middleware
@@ -44,17 +63,31 @@ function validateUserId(req, res, next) {
       req.getUserById = getUserById;
       next();
     } else {
-      res.status(404).json({ error: "User Does Not Exist" });
+      res.status(400).json({ error: "User Does Not Exist" });
     }
   };
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  return (req, res, next) => {
+    if (!req.body.name) {
+      res.status(400).json({ error: "Missing required name field" });
+    } else {
+      req.name = req.body.name;
+      next();
+    }
+  };
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  return (req, res, next) => {
+    if (!req.body.text) {
+      res.status(400).json({ error: "Invalid Post Or Update Object" });
+    } else {
+      req.text = req.body.text;
+      next();
+    }
+  };
 }
 
 module.exports = router;
