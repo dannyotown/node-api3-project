4c30f36dba74b39cda99e9ca3c087d3c8525ea11
Dtown <dannyotown@gmail.com>
2019-12-10 10:12:31 -0600

    middleware for get requests

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code away!
+const express = require("express");
+const users = require("./users/userRouter");
+
+const server = express();
+server.use(express.json());
+// Bring all our subroutes into the main application
+// (Remember, subroutes can have more children routers)
+server.use("/api/users", users);
+
+server.use((err, req, res, next) => {
+  console.log(err),
+    res.status(500).json({
+      message: "Internal error occured, please try again later!"
+    });
+});
+server.listen(4000, () => {
+  console.log("\n*** Server Running on http://localhost:4000 ***\n");
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,39 +1,52 @@
-const express = require('express');
+const express = require("express");
+const userDb = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
+  // do your magic!
+  const getUsers = await userDb.get();
+  res.send(getUsers);
+});
+
+router.get("/:id", validateUserId(), (req, res) => {
+  // do your magic!
+  res.status(200).send(req.getUserById);
+});
+
+router.get("/:id/posts", validateUserId(), async (req, res) => {
+  const getUserPost = await userDb.getUserPosts(req.getUserById.id);
+  res.status(200).send(getUserPost);
+});
+
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  return async (req, res, next) => {
+    const getUserById = await userDb.getById(req.params.id);
+    if (getUserById) {
+      req.getUserById = getUserById;
+      next();
+    } else {
+      res.status(404).json({ error: "User Does Not Exist" });
+    }
+  };
 }
 
 function validateUser(req, res, next) {
